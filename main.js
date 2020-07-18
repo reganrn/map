fe32c7c04b8daea4bf77a7136dfe5698e835aa00
regan <reganbca@gmail.com>
2020-07-18 16:20:48 +0530

    changes updated

--- main.js
+++ main.js
@@ -3,230 +3,427 @@ var regions=[
         "region_name": "Telangana",
         "region_code": "0",
         "population": 9794525,
-        "color": "#F1F1F1"
+        "color": "#F1F1F1",
+        "district": "tel"
     },
     {
         "region_name": "Jammu and Kashmir",
         "region_code": "1",
         "population": 5769750,
-        "color": "#F9F9F9"
+        "color": "#F9F9F9",
+        "district": "jam"
     },
     {
         "region_name": "West Bengal",
         "region_code": "2",
         "population": 5557276,
-        "color": "#5390d9"
+        "color": "#5390d9",
+        "district": "wes"
     },
     {
         "region_name": "Uttaranchal",
         "region_code": "3",
         "population": 4999932,
-        "color": "#DDDDDD"
+        "color": "#DDDDDD",
+        "district": "utc"
     },
     {
         "region_name": "Uttar Pradesh",
         "region_code": "4",
         "population": 4881756,
-        "color": "#72efdd"
+        "color": "#72efdd",
+        "district": "utp"
     },
     {
         "region_name": "Tripura",
         "region_code": "5",
         "population": 4377487,
-        "color": "#56cfe1"
+        "color": "#56cfe1",
+        "district": "tri"
     },
     {
         "region_name": "Tamil Nadu",
         "region_code": "6",
         "population": 4374052,
-        "color": "#72efdd"
+        "color": "#72efdd",
+        "district": "tn",
+        "districtlist": [
+            {
+            "district_code": "0",
+            "district_name": "Virudunagar"
+            },
+            {
+            "district_code": "1",
+            "district_name": "Theni"
+            },
+            {
+            "district_code": "2",
+            "district_name": "Madurai"
+            },
+            {
+            "district_code": "3",
+            "district_name": "Sivaganga"
+            },
+            {
+            "district_code": "4",
+            "district_name": "Nilgiris"
+            },
+            {
+            "district_code": "5",
+            "district_name": "Krishnagiri"
+            },
+            {
+            "district_code": "6",
+            "district_name": "Dharmapuri"
+            },
+            {
+            "district_code": "7",
+            "district_name": "Vellore"
+            },
+            {
+            "district_code": "8",
+            "district_name": "Tiruvannamalai"
+            },
+            {
+            "district_code": "9",
+            "district_name": "Dindigul"
+            },
+            {
+            "district_code": "10",
+            "district_name": "Salem"
+            },
+            {
+            "district_code": "11",
+            "district_name": "Perambalur"
+            },
+            {
+            "district_code": "12",
+            "district_name": "Namakkal"
+            },
+            {
+            "district_code": "13",
+            "district_name": "Tiruchirappalli"
+            },
+            {
+            "district_code": "14",
+            "district_name": "Karur"
+            },
+            {
+            "district_code": "15",
+            "district_name": "Erode"
+            },
+            {
+            "district_code": "16",
+            "district_name": "Coimbatore"
+            },
+            {
+            "district_code": "17",
+            "district_name": "Tiruppur"
+            },
+            {
+            "district_code": "18",
+            "district_name": "Ariyalur"
+            },
+            {
+            "district_code": "19",
+            "district_name": "Kancheepuram"
+            },
+            {
+            "district_code": "20",
+            "district_name": "Kancheepuram"
+            },
+            {
+            "district_code": "21",
+            "district_name": "Thiruvallur"
+            },
+            {
+            "district_code": "22",
+            "district_name": "Chennai"
+            },
+            {
+            "district_code": "23",
+            "district_name": "Viluppuram"
+            },
+            {
+            "district_code": "24",
+            "district_name": "Cuddalore"
+            },
+            {
+            "district_code": "25",
+            "district_name": "Chennai"
+            },
+            {
+            "district_code": "26",
+            "district_name": "Chennai"
+            },
+            {
+            "district_code": "27",
+            "district_name": "Nagappattinam"
+            },
+            {
+            "district_code": "28",
+            "district_name": "Thiruvarur"
+            },
+            {
+            "district_code": "29",
+            "district_name": "Thanjavur"
+            },
+            {
+            "district_code": "30",
+            "district_name": "Kanniyakumari"
+            },
+            {
+            "district_code": "31",
+            "district_name": "Tirunelveli"
+            },
+            {
+            "district_code": "32",
+            "district_name": "Thoothukkudi"
+            },
+            {
+            "district_code": "33",
+            "district_name": "Ramanathapuram"
+            },
+            {
+            "district_code": "34",
+            "district_name": "Ramanathapuram"
+            },
+            {
+            "district_code": "36",
+            "district_name": "Ramanathapuram"
+            },
+            {
+            "district_code": "37",
+            "district_name": "Ramanathapuram"
+            },
+            {
+            "district_code": "38",
+            "district_name": "Pudukkottai"
+            },
+            {
+            "district_code": "39",
+            "district_name": "Pudukkottai"
+            }    
+        ]
     },
     {
         "region_name": "Sikkim",
         "region_code": "7",
         "population": 4050803,
-        "color": "#5390d9"
+        "color": "#5390d9",
+        "district": "si"
     },
     {
         "region_name": "Rajasthan",
         "region_code": "8",
         "population": 3692828,
-        "color": "#56cfe1"
+        "color": "#56cfe1",
+        "district": "ra"
     },
     {
         "region_name": "Punjab",
         "region_code": "9",
         "population": 1958238,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "pu"
     },
     {
         "region_name": "Puducherry",
         "region_code": "10",
         "population": 1640379,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "pc"
     },
     {
         "region_name": "Orissa",
         "region_code": "11",
         "population": 1565127,
-        "color": "#56cfe1"
+        "color": "#56cfe1",
+        "district": "or"
     },
     {
         "region_name": "Nagaland",
         "region_code": "12",
         "population": 1545155,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "na"
     },
     {
         "region_name": "Mizoram",
         "region_code": "13",
         "population": 1312507,
-        "color": "#56cfe1"
+        "color": "#56cfe1",
+        "district": "mi"
     },
     {
         "region_name": "Meghalaya",
         "region_code": "14",
         "population": 1221860,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "me"
     },
     {
         "region_name": "Manipur",
         "region_code": "15",
         "population": 1039934,
-        "color": "#56cfe1"
+        "color": "#56cfe1",
+        "district": "mp"
     },
     {
         "region_name": "Maharashtra",
         "region_code": "16",
         "population": 886239,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "mha"
     },
     {
         "region_name": "Madhya Pradesh",
         "region_code": "17",
         "population": 576194,
-        "color": "#5390d9"
+        "color": "#5390d9",
+        "district": "map"
     },
     {
         "region_name": "Molise",
         "region_code": "18",
         "population": 313341,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "mol"
     },
     {
         "region_name": "Kerala",
         "region_code": "19",
         "population": 127844,
-        "color": "#5390d9"
+        "color": "#5390d9",
+        "district": "kl"
     },
     {
         "region_name": "Karnataka",
         "region_code": "20",
         "population": 127844,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "ka"
     },
     {
         "region_name": "Jharkhand",
         "region_code": "21",
         "population": 127844,
-        "color": "#5390d9"
+        "color": "#5390d9",
+        "district": "jh"
     },
     {
         "region_name": "Himachal Pradesh",
         "region_code": "22",
         "population": 127844,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "hp"
     },
     {
         "region_name": "Haryana",
         "region_code": "23",
         "population": 127844,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "har"
     },
     {
         "region_name": "Gujarat",
         "region_code": "24",
         "population": 127844,
-        "color": "#5390d9"
+        "color": "#5390d9",
+        "district": "guj"
     },
     {
         "region_name": "Goa",
         "region_code": "25",
         "population": 127844,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "goa"
     },
     {
         "region_name": "Delhi",
         "region_code": "26",
         "population": 127844,
-        "color": "#5390d9"
+        "color": "#5390d9",
+        "district": "del"
     },
     {
         "region_name": "Daman and Diu",
         "region_code": "27",
         "population": 127844,
-        "color": "#4ea8de"
+        "color": "#4ea8de",
+        "district": "dam"
     },
     {
         "region_name": "Val d'Aosta",
         "region_code": "28",
         "population": 127844,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "val"
     },
     {
         "region_name": "Chhattisgarh",
         "region_code": "29",
         "population": 127844,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "chh"
     },
     {
         "region_name": "Val d'Aosta",
         "region_code": "30",
         "population": 127844,
-        "color": "#4ea8de"
+        "color": "#4ea8de",
+        "district": "val"
     },
     {
         "region_name": "Bihar",
         "region_code": "31",
         "population": 127844,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "bih"
     },
     {
         "region_name": "Assam",
         "region_code": "32",
         "population": 127844,
-        "color": "#4ea8de"
+        "color": "#4ea8de",
+        "district": "ass"
     },
     {
         "region_name": "Arunachal Pradesh",
         "region_code": "33",
         "population": 127844,
-        "color": "#FFFFFF"
+        "color": "#FFFFFF",
+        "district": "arp"
     },
     {
         "region_name": "Andhra Pradesh",
         "region_code": "34",
         "population": 127844,
-        "color": "#80ffdb"
+        "color": "#80ffdb",
+        "district": "anp"
     },
     {
         "region_name": "Andaman and Nicobar",
         "region_code": "35",
         "population": 127844,
-        "color": "#64dfdf"
+        "color": "#64dfdf",
+        "district": "aan"
     }
 ];
+
+window.onload = function() {
+    localStorage.removeItem("currentID");
+};
 
 // Click
 const currentMap = document.querySelectorAll('.map path')
 for (const currentClick of currentMap) {
     currentClick.addEventListener('click', function(event) {
-        document.querySelector('.map-popup').classList.add("active");
-        document.querySelector('.popup-content h1').innerText = regions[this.id].region_name;
-        document.querySelector('.popup-content p').innerText = regions[this.id].population;
+        document.getElementById(regions[this.id].district).classList.add("active");     
+        document.querySelector('.map').classList.add("active");
+        localStorage.setItem("currentID", this.id); 
   })
 }
-
 
 for (const currentClick of currentMap) {
 // Mouseover
@@ -234,11 +431,12 @@ for (const currentClick of currentMap) {
         document.querySelector('.tooltip').classList.add("active");
         document.querySelector('.tooltip h2').innerText = regions[this.id].region_name;
         document.querySelector('.tooltip p').innerText = regions[this.id].population;
+        var district = regions[this.id].district;
         var tootTip = document.querySelector('.tooltip');
-        x = e.offsetX;
-        y = e.offsetY;
-        tootTip.style.left = 150 + x+"px";
-        tootTip.style.top = 50 + y+"px";
+        x = e.clientX;
+        y = e.clientY;
+        tootTip.style.left = (x + 20) + 'px';
+        tootTip.style.top = (y + 20) + 'px';
   })
 // mouseleave
   currentClick.addEventListener('mouseleave', function(e) {
@@ -246,11 +444,39 @@ for (const currentClick of currentMap) {
   });
 }
 
-document.querySelector('.map-popupclose').addEventListener('click', function(){
-    document.querySelector('.map-popup').classList.remove("active");
-});
+
+// Click
+const districtMap = document.querySelectorAll('.district-list path')
+for (const districtClick of districtMap) {
+    districtClick.addEventListener('click', function(event) {
+        var getCurrentID = localStorage.getItem("currentID");
+        var districkJson = regions[getCurrentID].districtlist;
+        console.log(this.id.replace(/[a-z]/g, ''))
+  })
+}
+
+for (const districtClick of districtMap) {
+// Mouseover
+        districtClick.addEventListener('mousemove', function(e) {
+        var getCurrentID = localStorage.getItem("currentID");
+        var districkJson = regions[getCurrentID].districtlist;
+        //console.log(districkJson[0].district_name)
+        document.querySelector('.tooltip').classList.add("active");
+        var districtID = this.id.replace(/[a-z]/g, '');
+        document.querySelector('.tooltip p').innerText = districkJson[districtID].district_name;
+        var tootTip = document.querySelector('.tooltip');
+        x = e.clientX;
+        y = e.clientY;
+        tootTip.style.left = (x + 20) + 'px';
+        tootTip.style.top = (y + 20) + 'px';
+  })
+// mouseleave
+districtClick.addEventListener('mouseleave', function(e) {
+    document.querySelector('.tooltip').classList.remove("active");
+  });
+}
+
 
 for(i = 0; i < regions.length; i++) {
-    console.log()
     document.getElementById(i).style.fill= regions[i].color
 }
